e862dd6f4bd36c055a1f22e0c58736cd1a80a9ea
Reiz Janto <betswanky@gmail.com>
2024-08-26 23:58:57 +0100

    Seems normal now abi

--- server.js
+++ server.js
@@ -360,7 +360,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start the Server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
